81365276e9b2546ab8398001c89ba10007f4d12c
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-07-13 23:47:24 +0800

    added new hero image to main page. Navbar changes color when scrolled

--- components/NavbarResponsive.tsx
+++ components/NavbarResponsive.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Sacramento } from "next/font/google";
 import { useRouter } from 'next/router';
 
@@ -9,19 +9,36 @@ const sacra = Sacramento({
 
 function NavbarResponsive() {
 
-  const [mobileMenuShowing, setMobileMenuShowing] = useState(false)
+  const [mobileMenuShowing, setMobileMenuShowing] = useState(false);
+  const [navbarTransparent, setNavbarTransparent] = useState(true);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY; // => scroll position
+      if (scrollPosition !== 0) {
+        setNavbarTransparent(false)
+      } else {
+        setNavbarTransparent(true)
+      }
+      console.log(scrollPosition);
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const router = useRouter();
   console.log(router.pathname)
 
-  return <nav className="bg-yellow-200 border-gray-200 w-full fixed top-0 z-50">
+  return <nav className={`${navbarTransparent? "bg-transparent": "bg-yellow-200"} transition-colors duration-300 border-gray-200 w-full fixed top-0 z-50`}>
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div className="flex flex-row justify-center items-center">
         <img src="/Logo_white border_no bg_no text.png" className="h-8 mr-3" alt="Flowbite Logo" />
         <span className={`self-center text-3xl whitespace-nowrap text-black ${sacra.className}`}>Jem Studio</span>
       </div>
 
-      <button onClick={(e)=>setMobileMenuShowing(!mobileMenuShowing)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+      <button onClick={(e) => setMobileMenuShowing(!mobileMenuShowing)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -30,7 +47,7 @@ function NavbarResponsive() {
       <div className={`hidden  w-full md:block md:w-auto`} id="navbar-default">
         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:border-gray-700">
           <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current={router.pathname=="/" ? "page" : "false"}>Home</a>
+            <a href="#" className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current={router.pathname == "/" ? "page" : "false"}>Home</a>
           </li>
 
           <li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,41 @@
 import type { NextPage } from "next";
+import { useRef } from "react";
+import Image from "next/image";
 
-import NavbarPopup from "../components/NavbarPopup";
-import Navbar from "../components/navbar";
 import NavbarResponsive from "../components/NavbarResponsive";
 import BioContainer from "./components/BioContainer";
 import TeachingMethodologyContainer from "./components/TeachingMethodologyContainer";
 import QuestionsContainer from "./components/questions-container";
 import Footer from "../components/footer";
-import Carousel from "./Carousel";
+import heroImage from '../public/images/Hau Tzen freeze frame.png'
+
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  fallback: ["serif"]
+})
 
 const MacBookAir2: NextPage = () => {
+  const mainContainerRef = useRef<HTMLDivElement>(null)
+
+
+
   return (
     <>
       <NavbarResponsive />
-      <main className="relative flex flex-col gap-12 md:gap-32 bg-white w-full overflow-hidden  text-left text-white font-kaisei-decol">
-
-        <Carousel />
+      <main onScroll={(e) => console.log('scrolling')} className="relative flex flex-col gap-12 md:gap-32 bg-white w-full overflow-hidden  text-left text-white font-kaisei-decol">
+        <div className="relative w-full">
+          <Image alt="banner image" className="object-cover w-full h-screen brightness-[0.6] contrast-75 " src={heroImage} />
+          <div className={`${playfair.className} absolute top-0 w-full h-full text-white z-30 flex justify-center items-center`}>
+            <div>
+              <p className="text-[6rem]">Learn Music</p>
+              <p className="text-6xl text-right -mr-10">Your Way</p>
+            </div>
+          </div>
+        </div>
         <img
-          className="relative w-[555.32px] h-[1090.58px] hidden"
+          className="absolute w-[555.32px] h-[1090.58px] z-0 hidden"
           alt=""
           src="/subtract.svg"
         />
