64ad8127c4a03e975c90996ac7c39beb4fcf4051
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-07-23 00:41:52 +0800

    left align address in /about. Improved footer mobile syling

--- app/about/Address.tsx
+++ app/about/Address.tsx
@@ -11,7 +11,7 @@ function Address() {
     return <section className="flex flex-col lg:flex-row justify-center items-center gap-10 lg:gap-20">
 
         <iframe className="aspect-[4/3] w-5/6 object-cover lg:w-1/3 h-auto" loading="lazy" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJwf6r38IX2jERHmVxmBQFHZU&key=AIzaSyCZUygOzVi4VndXok5wwLGGedze-gx2KJI"></iframe>
-        <div className="flex justify-center items-center aspect-[4/3] w-5/6 md:w-1/2 lg:w-1/3 h-auto">
+        <div className="flex justify-start lg:justify-center items-center aspect-[4/3] w-5/6 md:w-1/2 lg:w-1/3 h-auto">
             <div className="flex flex-col justify-center items-start">
                 <h2 className={`${playfair.className} text-2xl lg:text-4xl text-black`}>Our Studio</h2>
                 <span className="w-16 border-t-2 border-t-stone-500 h-1 mt-8 mb-2"></span>
--- components/footer.tsx
+++ components/footer.tsx
@@ -9,43 +9,46 @@ const socials = [
   { src: "/footerIcons/xhsIcon.svg", link: "https://www.xiaohongshu.com/user/profile/6175fe6400000000020277b5?xhsshare=CopyLink&appuid=6175fe6400000000020277b5&apptime=1689771888" }
 ]
 
+const links = [
+  { text: "Home", href: "/" },
+  { text: "About", href: "/about" },
+]
+
 function Footer() {
   function scrollToTop() {
     window.scrollTo(0, 0)
   }
   return (
-    <div className="self-stretch bg-zinc-700 flex flex-row py-3  px-6 md:py-5 md:px-9 lg:px-20 box-border items-center justify-between text-left text-xl text-white font-inter">
-      <div className="flex flex-col items-start">
-        <div className="flex flex-row gap-x-2 items-start justify-between">
-          {socials.map((social) => (
-            <Link href={social.link} key={social.src}>
-              <img
-                className="relative aspect-square h-8 md:h-12 overflow-hidden shrink-0"
-                alt=""
-                src={social.src}
-              />
-            </Link>
-
-          ))}
-
-        </div>
-        <span className="w-16 border-t border-t-stone-200 mt-4 mb-2"></span>
-        <span className="text-xs">&copy;Rondo Studio</span>
-      </div>
-
-
-      <div className="text-sm md:text-base flex flex-col gap-y-3 items-center text-stone-200">
-        <div className="flex gap-x-3">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-        </div>
+    <footer className="self-stretch bg-zinc-700 flex flex-col items-center md:py-5 md:px-9 lg:px-20 box-border text-left text-xl text-white font-inter">
+      <div className="text-sm md:text-base flex flex-col items-center text-stone-200 w-full">
+        {links.map((link) => {
+          return <>
+            <Link className="h-12 flex justify-center items-center" href={link.href}>{link.text}</Link>
+            <hr className="border-t border-t-stone-500 h-1 w-full" />
+          </>
+        })}
         <button onClick={() => {
           scrollToTop()
           console.log('clicked')
-        }} className="relative ">Back to Top</button>
+        }} className="h-12 flex justify-center items-center">Back to Top</button>
+        <hr className="border-t border-t-stone-500 h-1 w-full" />
       </div>
-    </div>
+      <div className="my-6 flex flex-row gap-x-6 items-start justify-between">
+        {socials.map((social) => (
+          <Link href={social.link} key={social.src}>
+            <img
+              className="relative aspect-square h-10 md:h-12 overflow-hidden shrink-0"
+              alt=""
+              src={social.src}
+            />
+          </Link>
+        ))}
+      </div>
+      <span className="text-xs text-stone-400 mb-8">&copy;2023 Rondo Studio</span>
+    </footer>
   );
 };
 
 export default Footer;
+
+
